b29a98d1a7f5b87a1e96f44f09064ef3458e8910
Laken Harville <rlakenharville@gmail.com>
2022-04-17 19:20:04 -0500

    Initial commit

---
+++ ListDataAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryExercisePatternPart2
+{
+    class ListDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>
+        {
+            new Product(){Name = "Shoes", Price = 250},
+            new Product(){Name = "Shirt", Price = 20},
+            new Product(){Name = "Pants", Price = 50},
+            new Product(){Name = "Belt", Price = 34},
+        };
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from List Data Access.");
+
+                return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data from a List Data Access");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace FactoryExercisePatternPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            bool correctInput;
+            do {
+                Console.Clear();
+                correctInput = true;
+
+                Console.WriteLine("Pick a data base.");
+                Console.WriteLine("Type in sql, mongo, or list.");
+
+
+                userInput = Console.ReadLine();
+
+                if(userInput != "sql" && userInput != "mongo" && userInput != "list")
+                {
+                    correctInput = false;
+                    Console.WriteLine("Invalid input.");
+                    Thread.Sleep(1000);
+                }
+            } while (!correctInput);
+            IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+            var products = db.LoadData();
+            db.SaveData();
+
+            foreach(var item in products)
+            {
+                Console.WriteLine($"Name: {item.Name}. Price: {item.Price}");
+            }
+        }
+    }
+}
